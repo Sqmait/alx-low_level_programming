c01f6ff534207dd9512a2815a87255e0823cc140
Sqmait <quasartan030@gmail.com>
2022-12-08 16:13:06 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ * /
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle"):
+	 return (0);
+ }
+

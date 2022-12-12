cee4fd955256f372d44b0a3e95738a391fe671a3
Sqmait <quasartan030@gmail.com>
2022-12-11 22:43:48 -0800

    When I was having that alphabet soup I never thought that it would play off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints the alphabet in lowercase followed by a new line
+ * main - a program that prints the alphabet in lowercase
  *
  * Return: return 0 (success)
  */
@@ -11,7 +11,7 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-			if (( != 'q' && c != 'e') && c <= 'z')
+			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
 			c++;
 		}

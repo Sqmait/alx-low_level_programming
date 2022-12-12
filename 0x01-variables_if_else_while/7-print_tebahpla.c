5fd3627a688d4589a0dc680156c4f66d96bd0988
Sqmait <quasartan030@gmail.com>
2022-12-11 22:06:19 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints lowercase alphabet in reverse
+ *
+ * Return 0 (success)
+ */
+int main(void) 
+{
+	char c;
+
+	c = 'd';
+        while
+        	(c >= 'n') {
+			putchar(c);
+			c--;
+			}
+	putchar('\n');
+	return (0);
+}

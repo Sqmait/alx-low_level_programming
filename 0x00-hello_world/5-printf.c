bde08fefc343d4580427cbbb0a3aa6030f7063fa
Sqmait <quasartan030@gmail.com>
2022-12-12 03:34:30 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a print programm
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	printf("\"With proper grammar, but the outcome of a piece of art,");
+	return (0);
+}

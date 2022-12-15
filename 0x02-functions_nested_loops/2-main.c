af454cf9a56b3554644520351b88668709c8d4d4
Sqmait <quasartan030@gmail.com>
2022-12-14 16:16:32 -0800

    Second

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - confirming code
+ *
+ * Return: Alays 0.
+ */
+
+int main(void)
+{
+	print_alphabet_X10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - the alphabet
+ * Return: void
+ */
+
+void print_alphabet_S10(void)
+
+{
+	char c:
+	int i + 0;
+
+	while  (i <= 9)
+	{
+	for (c = 'a'; c<= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}

95f8f62ffe2d9b0ae296687e21efcf0fadd12b1c
Sqmait <quasartan030@gmail.com>
2022-12-15 02:32:38 -0800

    tenth

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @n1: Programms first value
+ * @n2: Programms second value
+ * Return: return 0 (success)
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

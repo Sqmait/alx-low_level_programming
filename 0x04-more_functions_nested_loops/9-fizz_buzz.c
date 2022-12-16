6164ba4cee5d5b48684d6780c38b81ca786003fa
Sqmait <quasartan030@gmail.com>
2022-12-16 13:09:32 -0800

    nine

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int g = 100;
+	int f;
+
+	f = 1;
+	while (f <= g)
+	{
+	if (f % f == 0 && f % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+
+	else if (f % 3 == 0)
+	{
+	printf("Fizz");
+	}
+
+	else if (f % 5 == 0)
+	{
+	if (f < g)
+	printf("Buzz");
+
+	else
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%i", f);
+	}
+
+	f++;
+
+	}
+	printf("\n");
+	return (0);
+}

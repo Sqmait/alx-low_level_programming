a563169773eea5ea020334eb0f6a4aea3f6b1bd0
Sqmait <quasartan030@gmail.com>
2022-12-16 03:11:39 -0800

    first proj

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks numbers 0 - 9
+ * @c: number to check
+ * Return: return 0 (success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+}

b7391789483e55c0657f0c23efdc08a71fce76ae
Sqmait <quasartan030@gmail.com>
2022-12-16 11:13:17 -0800

    header file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,14 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	n = 0;
+	while (i < 10)
 	{
-	_putchar(c);
+	_putchar (i + '0');
+	i++;
 	}
-	_putchar('\n');
-}
+
+	_putchar ('\n');
+}	

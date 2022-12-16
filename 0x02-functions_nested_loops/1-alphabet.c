a69a7f7b8b791a823c86428040f64ee2176c89b3
Sqmait <quasartan030@gmail.com>
2022-12-15 18:26:32 -0800

    third

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,19 +5,8 @@
  * Return: return 0 (success)
  */
 
-void print_alphabet(void)
-
+int main(void)
 {
-	char c;
-	int i = 0;
-
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
-	}
+	print_alphabet();
+	return (0);
 }

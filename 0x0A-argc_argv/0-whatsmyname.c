3a92e93e73add9be2b0744a4b23c191d1e495ef0
Sqmait <quasartan030@gmail.com>
2023-01-10 04:06:05 -0800

    project 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @m: prototype
+ * @n: prototpe
+ * Return: return 0 (success)
+ */
+int main(int m, char *n[])
+{
+	printf("%s\n", n[0]);
+	(void)m;
+	return (0);
+}

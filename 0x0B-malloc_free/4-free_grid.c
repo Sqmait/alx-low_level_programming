01c67518d2f9d48e713b805a0fa9adadea0ca0f0
Sqmait <quasartan030@gmail.com>
2023-01-12 18:48:05 -0800

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - 
- * @grid:
- * @height:
- * Return:
+ * free_grid - a function that frees..
+ * @grid:prototype
+ * @height: prototype
+ * Return: return 0 (success)
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+	int n;
 
-	for (i = 0; i < height; i++)
+	for (n = 0; n < height; n++)
 	{
-		free(grid[i]);
+		free(grid[n]);
 	}
 	free(grid);
 }
